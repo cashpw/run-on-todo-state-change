523851603251b2b256c000a5b9d83f480f7b67af
Cash Prokop-Weaver <cashbweaver@gmail.com>
2025-11-23 10:43:47 -0800

    feat: Add --STATE fn handling
    **What?**
    
    1. Add arbitrary `run-on-state-change--STATE` handlers where STATE can
    be any headline state; even custom ones! For example: TODO or DONE.
    
    **Why?**
    
    The existing property-based method (`RUN_ON_*`) are useful for
    per-headine hooks. These functions make it simple to do something for
    _any_ headine which is set to a given state.

--- run-on-todo-state-change.el
+++ run-on-todo-state-change.el
@@ -42,32 +42,60 @@
   :group 'run-on-todo-state-change
   :type 'string)
 
+(defcustom run-on-todo-state-change--fn-prefix "run-on-todo-state-change--"
+  "Prefix for per-state functions.
+For example: `run-on-todo-state-change--todo'"
+  :group 'run-on-todo-state-change
+  :type 'string)
+
+(defun run-on-todo-state-change--TODO ()
+  "Run when a todo state is set to \"TODO\"."
+  ;; You should override this.
+  )
+
+(defun run-on-todo-state-change--DONE ()
+  "Run when a todo state is set to \"DONE\"."
+  ;; You should override this.
+  )
+
 (defun run-on-todo-state-change-prop (state)
   "Return run-on- org-mode element property based on STATE."
   (concat run-on-todo-state-change--prop-prefix (upcase state)))
 
 (defun run-on-todo-state-change ()
   "Run arbitrary code on org-mode TODO state change."
+  (run-on-todo-state-change--run-prop-fns)
+  (run-on-todo-state-change--run-fns))
+
+(defun run-on-todo-state-change--run-fns ()
+  "Run arbitrary code on org-mode TODO state change."
   (when-let* ((new-state org-state)
-              (fns-string
-               (org-extras-get-property
-                (point) (run-on-todo-state-change-prop new-state)))
-              (fn-strings
-               (let ((paren-counter 0)
-                     (previous-counter 0))
-                 (mapcar
-                  #'string-join
-                  (-split-when
-                   (lambda (char)
-                     (setf previous-counter paren-counter)
-                     (cond
-                      ((string= char "(")
-                       (cl-incf paren-counter))
-                      ((string= char ")")
-                       (cl-decf paren-counter)))
-                     (= 0 paren-counter previous-counter))
-                   (butlast
-                    (cdr (split-string (format "%s " fns-string) ""))))))))
+              (fn (intern (concat run-on-todo-state-change--fn-prefix new-state))))
+    (funcall fn)))
+
+(defun run-on-todo-state-change--run-prop-fns ()
+  "Run functions set through \"RUN_ON_\" properties."
+  (when-let*
+      ((new-state org-state)
+       (fns-string
+        (org-extras-get-property
+         (point) (run-on-todo-state-change-prop new-state)))
+       (fn-strings
+        (let ((paren-counter 0)
+              (previous-counter 0))
+          (mapcar
+           #'string-join
+           (-split-when
+            (lambda (char)
+              (setf previous-counter paren-counter)
+              (cond
+               ((string= char "(")
+                (cl-incf paren-counter))
+               ((string= char ")")
+                (cl-decf paren-counter)))
+              (= 0 paren-counter previous-counter))
+            (butlast
+             (cdr (split-string (format "%s " fns-string) ""))))))))
     (dolist (fn-string fn-strings)
       (eval (read fn-string)))))
 
